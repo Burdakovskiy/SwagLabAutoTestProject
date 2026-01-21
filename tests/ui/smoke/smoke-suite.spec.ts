33e6cce4fad80bc145ff7e2cb62ad9d520a13693
Burdakovskiy <www.deluxesss@gmail.com>
2026-01-21 12:57:45 +0100

    Add product import

--- tests/ui/smoke/smoke-suite.spec.ts
+++ tests/ui/smoke/smoke-suite.spec.ts
@@ -1,23 +1,26 @@
-import { test, expect } from "../../fixtures/ui.fixtures";
-import { CHECKOUT_DATA } from "../../../src/testData/checkout"
+import { test, expect } from '../../fixtures/ui.fixtures';
+import { CHECKOUT_DATA } from '../../../src/testData/checkout';
+import { PRODUCT } from '../../../src/testData/product';
 
-const PRODUCT = "Sauce Labs Backpack";
+test.describe('@ui @smoke Smoke suite', () => {
+  test('@ui @smoke Login happy path', async ({ loggedInUser: _loggedInUser, inventoryPage }) => {
+    await expect(inventoryPage.title).toContainText('Products');
+  });
 
-test.describe("@ui @smoke Smoke suite", () => {
-    test("@ui @smoke Login happy path", async ({ loggedInUser: _loggedInUser, inventoryPage }) => {
-        await expect(inventoryPage.title).toContainText("Products");
-    });
+  test('@ui @smoke Add to cart', async ({ loggedInUser: _loggedInUser, checkout }) => {
+    await checkout.addItemAndOpenCart(PRODUCT.name);
+  });
 
-    test("@ui @smoke Add to cart", async ({ loggedInUser: _loggedInUser, checkout }) => {
-        await checkout.addItemAndOpenCart(PRODUCT);
-    });
+  test('@ui @smoke Checkout end-to-end', async ({
+    loggedInUser: _loggedInUser,
+    checkout,
+    cartPage,
+  }) => {
+    const { firstName, lastName, postalCode } = CHECKOUT_DATA.validCustomer;
 
-    test("@ui @smoke Checkout end-to-end", async ({ loggedInUser: _loggedInUser, checkout, cartPage }) => {
-        const { firstName, lastName, postalCode } = CHECKOUT_DATA.validCustomer;
+    await checkout.addItemAndOpenCart(PRODUCT.name);
+    await cartPage.proceedToCheckout();
 
-        await checkout.addItemAndOpenCart(PRODUCT);
-        await cartPage.proceedToCheckout();
-
-        await checkout.checkoutHappyPath(firstName, lastName, postalCode);
-    });
+    await checkout.checkoutHappyPath(firstName, lastName, postalCode);
+  });
 });
